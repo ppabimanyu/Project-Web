96399c9853dbc09eb32e8f4b2481006961ca3c31
Abimanyu <ppabimanyu@gmail.com>
2021-06-05 11:24:29 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/home', function () {
     return view('index');
 });
+
+// Route::get('/home', function () {
+//     return view('index');
+// });
 
 Route::get('/portfolio-details', function () {
     return view('portfolio-details');

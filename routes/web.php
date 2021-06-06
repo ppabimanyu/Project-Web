6fbd020757cb85b7468a5af73db677c5de7c61b9
Abimanyu <ppabimanyu@gmail.com>
2021-06-06 20:25:59 +0700

    Login&Register

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,13 @@ Route::get('/', function () {
     return view('index');
 });
 
+Route::get('/login', function () {
+    return view('login');
+});
+Route::get('/register', function () {
+    return view('register');
+});
+
 // Route::get('/event', function () {
 //     return view('index#services');
 // });

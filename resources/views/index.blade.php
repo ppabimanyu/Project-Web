0513ae15f50428f2a4084dba7b85d03181bf61e9
Abimanyu <ppabimanyu@gmail.com>
2021-07-20 12:55:14 +0700

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -26,34 +26,27 @@
 
   <!-- Template Main CSS File -->
   <link href="/assets/css/style.css" rel="stylesheet">
-
-  <!-- =======================================================
-  * Template Name: Ninestars - v4.3.0
-  * Template URL: https://bootstrapmade.com/ninestars-free-bootstrap-3-theme-for-creative/
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
 </head>
 
 <body>
-
   <!-- ======= Header ======= -->
   <header id="header" class="fixed-top d-flex align-items-center">
     <div class="container d-flex align-items-center justify-content-between">
-
+      <!-- Logo -->
       <div class="logo">
-        <h1 class="text-light"><a href="index.html"><span>LiveIn</span></a></h1>
+        <h1 class="text-light"><a href="/"><span>LiveIn</span></a></h1>
         <!-- Uncomment below if you prefer to use an image logo -->
         <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
       </div>
-
+      <!-- End Logo -->
+      <!-- Navbar -->
       <nav id="navbar" class="navbar">
         <ul>
           @auth
           <!-- <li><a class="nav-link scrollto " href="#carouselExampleControls">Home</a></li> -->
           <li><a class="nav-link scrollto" href="/">Event</a></li>
           @else
-          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+          <li><a class="nav-link scrollto " href="#hero">Home</a></li>
           <li><a class="nav-link scrollto" href="#about">About Us</a></li>
           <li><a class="nav-link scrollto" href="#services">Event</a></li>
           @endauth
@@ -64,22 +57,50 @@
           <li><a class="nav-link scrollto" href="#team">Team</a></li>
           @if (Route::has('login'))
           @auth
+          @if(auth()->user()->role === 'admin')
+          <li><a class="getstarted scrollto" href="{{ url('/admin') }}">Dashboard</a></li>
+          @else
           <li><a class="getstarted scrollto" href="{{ url('/dashboard') }}">Dashboard</a></li>
+          @endif
           @else
           <li><a class="getstarted scrollto" href="{{ route('register') }}">Get Started</a></li>
           @endauth
           @endif
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
-      </nav><!-- .navbar -->
-
+      </nav>
+      <!-- End Navbar -->
     </div>
   </header><!-- End Header -->
 
-  <!-- ======= Hero Section ======= -->
   @auth
   <div class="py-5"></div>
+  <!-- <div id="carouselExampleControls" class="carousel slide mt-2 d-none d-md-block d-lg-block" data-bs-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <a href="#services">
+          <img src="/assets/img/about-img.svg" height="600px" class="d-block w-100" alt="...">
+        </a>
+      </div>
+      @foreach($events as $event)
+      <div class="carousel-item">
+        <a href="/details/{{$event->id}}">
+          <img src="/storage/images/{{$event->img}}" height="600px" class="d-block w-55 mx-auto" style="object-fit:cover" alt="...">
+        </a>
+      </div>
+      @endforeach
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div> -->
   @else
+
   <section id="hero" class="d-flex align-items-center">
     <div class="container">
       <div class="row gy-4">
@@ -87,7 +108,7 @@
           <h1>Bettter digital experience with LiveIn</h1>
           <h2>Make your live broadcast reach many people</h2>
           <div>
-            <a href="#about" class="btn-get-started scrollto">Get Started</a>
+            <a href="{{ route('register') }}" class="btn-get-started scrollto">Get Started</a>
           </div>
         </div>
         <div class="col-lg-6 order-1 order-lg-2 hero-img">
@@ -95,13 +116,11 @@
         </div>
       </div>
     </div>
-  </section><!-- End Hero -->
+  </section>
 
   <main id="main">
-    <!-- ======= About Section ======= -->
     <section id="about" class="about">
       <div class="container">
-
         <div class="row justify-content-between">
           <div class="col-lg-5 d-flex align-items-center justify-content-center about-img">
             <img src="/assets/img/about-img.svg" class="img-fluid" alt="" data-aos="zoom-in">
@@ -114,13 +133,13 @@
             <div class="row">
               <div class="col-md-6" data-aos="fade-up" data-aos-delay="100">
                 <i class="bx bx-receipt"></i>
-                <h4>Kenapa harus menggunakan LiveIn?</h4>
-                <p>Karena dengan menggunakan LiveIn, event yang kita buat jadi semakin ramai pengunjung atau pengikutnya.</p>
+                <h4>Corporis voluptates sit</h4>
+                <p>Consequuntur sunt aut quasi enim aliquam quae harum pariatur laboris nisi ut aliquip</p>
               </div>
               <div class="col-md-6" data-aos="fade-up" data-aos-delay="200">
                 <i class="bx bx-cube-alt"></i>
-                <h4>Bagaimana LiveIn terbentuk?</h4>
-                <p>LiveIn terbentuk karena adanya ide dari kelompok kami, yang dimana juga ini merupakan final project dari tugas kami yang diberikan bapak ibu dosen.</p>
+                <h4>Ullamco laboris nisi</h4>
+                <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt</p>
               </div>
             </div>
           </div>
@@ -128,10 +147,7 @@
       </div>
     </section>
     @endauth
-    <!-- End About Section -->
 
-
-    <!-- ======= Services Section ======= -->
     <section id="services" class="services section-bg">
       <div class="container" data-aos="fade-up">
 
@@ -148,14 +164,16 @@
             <input type="text" name="keyword" class="form-control rounded-pill fs-5" id="search" placeholder="Search" style="border-color: transparent;">
           </div>
         </form>
+        <!-- End Searching -->
 
-        <div class="row" data-aos="fade-up" data-aos-delay="100">
+        <div class=" row" data-aos="fade-up" data-aos-delay="100">
           <div class="col-lg-12">
             <ul id="portfolio-flters">
               <li data-filter="*" class="filter-active">All</li>
               <li data-filter=".Seminar">Seminar</li>
               <li data-filter=".Kesehatan">Kesehatan</li>
               <li data-filter=".Gaming">Gaming</li>
+              <li data-filter=".Other">Other</li>
             </ul>
           </div>
         </div>
@@ -165,16 +183,27 @@
           <div class="col-6 col-sm-6 col-md-4 col-lg-3 align-items-stretch portfolio-item {{$event->category}}">
             <a href="/details/{{$event->id}}" class="link-detail">
               <div class="icon-box">
+                <div class="d-flex p-2">
+                  @if(((DB::table('users')->where('id', $event->id_user)->first())->profile_photo_path) === null)
+                  <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                    <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+                  </svg>
+                  @else
+                  <img src="storage/{{(DB::table('users')->where('id', $event->id_user)->first())->profile_photo_path}}" class="rounded-circle" alt="" style="width:30px; height:30px">
+                  @endif
+                  <p class="mt-1 ms-2">{{(DB::table('users')->where('id', $event->id_user)->first())->name}}</p>
+                </div>
                 @if(($event->img)===null)
                 <img src="/assets/img/about-img.svg" class="rounded-3" alt="">
                 @else
                 <img src="/storage/images/{{$event->img}}" class="rounded-3" alt="/assets/img/about-img.svg">
                 @endif
-                <h4 class="title mt-2">{{$event->title}}</h4>
+                <h4 class="title mt-2 text-break" data-bs-toggle="tooltip" data-bs-placement="top" title="{{$event->title}}">{{$event->title}}</h4>
                 <p class="description">
                   <i class="bi bi-bookmark-fill"> {{$event->category}}</i><br>
-                  <i class="bi bi-stopwatch"> {{$event->time}}</i><br>
-                  <i class="bi bi-calendar-event"> {{$event->date}}</i><br>
+                  <i class="bi bi-stopwatch"> {{\Carbon\Carbon::parse($event->time)->format('h:i A')}}</i><br>
+                  <i class="bi bi-calendar-event"> {{\Carbon\Carbon::parse($event->date)->format('l, j F Y')}}</i><br>
                   <i class="bi bi-geo-alt-fill"> {{$event->platform}}</i>
                 </p>
                 <hr class="mb-2">
@@ -185,10 +214,10 @@
           @endforeach
         </div>
       </div>
-    </section><!-- End Services Section -->
+    </section>
 
-    @auth
     <!-- ======= About Section ======= -->
+    @auth
     <section id="about" class="about">
       <div class="container">
 
@@ -217,20 +246,16 @@
         </div>
       </div>
     </section>
-    @else
     @endauth
 
     <!-- ======= F.A.Q Section ======= -->
     <section id="faq" class="faq section-bg">
       <div class="container" data-aos="fade-up">
-
         <div class="section-title">
           <h2>F.A.Q</h2>
           <p>Frequently Asked Questions</p>
         </div>
-
         <ul class="faq-list" data-aos="fade-up" data-aos-delay="100">
-
           <li>
             <div data-bs-toggle="collapse" class="collapsed question" href="#faq1">Bagaimana cara mendaftar di LiveIn? <i class="bi bi-chevron-down icon-show"></i><i class="bi bi-chevron-up icon-close"></i></div>
             <div id="faq1" class="collapse" data-bs-parent=".faq-list">
@@ -239,7 +264,6 @@
               </p>
             </div>
           </li>
-
           <li>
             <div data-bs-toggle="collapse" href="#faq2" class="collapsed question">Apakah kita memasang event di LiveIn diharuskan membayar? <i class="bi bi-chevron-down icon-show"></i><i class="bi bi-chevron-up icon-close"></i></div>
             <div id="faq2" class="collapse" data-bs-parent=".faq-list">
@@ -248,7 +272,6 @@
               </p>
             </div>
           </li>
-
           <li>
             <div data-bs-toggle="collapse" href="#faq3" class="collapsed question">Bagaimana jika ingin menghapus event yang sudah kita posting di LiveIn? <i class="bi bi-chevron-down icon-show"></i><i class="bi bi-chevron-up icon-close"></i></div>
             <div id="faq3" class="collapse" data-bs-parent=".faq-list">
@@ -257,7 +280,6 @@
               </p>
             </div>
           </li>
-
           <li>
             <div data-bs-toggle="collapse" href="#faq4" class="collapsed question">Apakah kita bisa memasang lebih dari satu jadwal event di LiveIn? <i class="bi bi-chevron-down icon-show"></i><i class="bi bi-chevron-up icon-close"></i></div>
             <div id="faq4" class="collapse" data-bs-parent=".faq-list">
@@ -266,7 +288,6 @@
               </p>
             </div>
           </li>
-
           <li>
             <div data-bs-toggle="collapse" href="#faq5" class="collapsed question">Bagaimana cara kerja LiveIn? <i class="bi bi-chevron-down icon-show"></i><i class="bi bi-chevron-up icon-close"></i></div>
             <div id="faq5" class="collapse" data-bs-parent=".faq-list">
@@ -275,7 +296,6 @@
               </p>
             </div>
           </li>
-
           <li>
             <div data-bs-toggle="collapse" href="#faq6" class="collapsed question">Bagaimana cara mengatasi error pada saat mendaftar di LiveIn? <i class="bi bi-chevron-down icon-show"></i><i class="bi bi-chevron-up icon-close"></i></div>
             <div id="faq6" class="collapse" data-bs-parent=".faq-list">
@@ -284,23 +304,19 @@
               </p>
             </div>
           </li>
-
         </ul>
-
       </div>
-    </section><!-- End F.A.Q Section -->
+    </section>
+    <!-- End F.A.Q Section -->
 
     <!-- ======= Team Section ======= -->
     <section id="team" class="team">
       <div class="container">
-
         <div class="section-title" data-aos="fade-up">
           <h2>Team</h2>
           <p>Our team is always here to help</p>
         </div>
-
         <div class="row">
-
           <div class="col" data-aos="zoom-in" data-aos-delay="100">
             <div class="member">
               <img src="/assets/img/team/mita.jpeg" class="img-fluid" alt="">
@@ -318,7 +334,6 @@
               </div>
             </div>
           </div>
-
           <div class="col" data-aos="zoom-in" data-aos-delay="200">
             <div class="member">
               <img src="/assets/img/team/rifky.jpeg" class="img-fluid" alt="">
@@ -336,25 +351,23 @@
               </div>
             </div>
           </div>
-
           <div class="col" data-aos="zoom-in" data-aos-delay="300">
             <div class="member">
-              <img src="/assets/img/team/team-3.jpg" class="img-fluid" alt="">
+              <img src="/assets/img/team/Abim.jpg" class="img-fluid" alt="">
               <div class="member-info">
                 <div class="member-info-content">
                   <h4>Putra Prasessia Abimanyu</h4>
                   <span>CTO</span>
                 </div>
                 <div class="social">
-                  <a href=""><i class="bi bi-twitter"></i></a>
-                  <a href=""><i class="bi bi-facebook"></i></a>
-                  <a href=""><i class="bi bi-instagram"></i></a>
-                  <a href=""><i class="bi bi-linkedin"></i></a>
+                  <a href="https://twitter.com/PpAbimanyu" target="blank"><i class="bi bi-twitter"></i></a>
+                  <a href="https://web.facebook.com/abimanyu.ppa/" target="blank"><i class="bi bi-facebook"></i></a>
+                  <a href="https://www.instagram.com/abimanyu_ppa/" target="blank"><i class="bi bi-instagram"></i></a>
+                  <a href="https://www.linkedin.com/in/putra-prassiesa-abimanyu-140336213/" target="blank"><i class="bi bi-linkedin"></i></a>
                 </div>
               </div>
             </div>
           </div>
-
           <div class="col" data-aos="zoom-in" data-aos-delay="400">
             <div class="member">
               <img src="/assets/img/team/rian2.jpeg" class="img-fluid" alt="">
@@ -372,7 +385,6 @@
               </div>
             </div>
           </div>
-
           <div class="col" data-aos="zoom-in" data-aos-delay="300">
             <div class="member">
               <img src="/assets/img/team/anton.jpeg" class="img-fluid" alt="">
@@ -392,15 +404,29 @@
           </div>
         </div>
       </div>
-    </section><!-- End Team Section -->
-  </main><!-- End #main -->
+    </section>
+    <!-- End Team Section -->
+  </main>
+  <!-- End #main -->
 
   <!-- ======= Footer ======= -->
   <footer id="footer">
+    <!-- <div class="footer-newsletter">
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-lg-6">
+              <h4>Join Our Newsletter</h4>
+              <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
+              <form action="" method="post">
+                <input type="email" name="email"><input type="submit" value="Subscribe">
+              </form>
+            </div>
+          </div>
+        </div>
+      </div> -->
     <div class="footer-top">
       <div class="container">
         <div class="row">
-
           <div class="col-lg-4 col-md-6 footer-contact">
             <h3>Ninestars</h3>
             <p>
@@ -411,7 +437,6 @@
               <strong>Email:</strong> LiveInStream@gmail.com<br>
             </p>
           </div>
-
           <div class="col-lg-4 col-md-6 footer-links">
             <h4>Useful Links</h4>
             <ul>
@@ -422,7 +447,6 @@
               <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
             </ul>
           </div>
-
           <div class="col-lg-4 col-md-6 footer-links">
             <h4>Our Social Networks</h4>
             <p>Agar kalian tidak ketinggalan berita, follow social network kita untuk informasi terbaru</p>
@@ -434,24 +458,19 @@
               <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
             </div>
           </div>
-
         </div>
       </div>
     </div>
-
     <div class="container py-4">
       <div class="copyright">
         &copy; Copyright <strong><span>Ninestars</span></strong>. All Rights Reserved
       </div>
       <div class="credits">
-        <!-- All the links in the footer should remain intact. -->
-        <!-- You can delete the links only if you purchased the pro version. -->
-        <!-- Licensing information: https://bootstrapmade.com/license/ -->
-        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/ninestars-free-bootstrap-3-theme-for-creative/ -->
         Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
       </div>
     </div>
-  </footer><!-- End Footer -->
+  </footer>
+  <!-- End Footer -->
 
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
@@ -465,7 +484,6 @@
 
   <!-- Template Main JS File -->
   <script src="/assets/js/main.js"></script>
-
 </body>
 
 </html>
